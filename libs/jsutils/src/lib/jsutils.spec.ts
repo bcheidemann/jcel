4a975601273c24599c527bb80251c99281a4c778
bcheidemann <ben@heidemann.co.uk>
2021-06-10 11:12:59 +0100

    feat: analyse cell dependencies

--- libs/jsutils/src/lib/jsutils.spec.ts
+++ libs/jsutils/src/lib/jsutils.spec.ts
@@ -1,7 +1,79 @@
-import { jsutils } from './jsutils';
+import { analyseCellDependancies } from './jsutils';
+
+function containsMany<T>(array: Array<T>, items: Array<T>) {
+  expect(array).toHaveLength(items.length);
+  for (const item of items) {
+    expect(array).toContain(item);
+  }
+}
+
+function cellWithCode(code: string) {
+  const { dependsOn, dependingOn } = analyseCellDependancies(code);
+  const expectFuncs = {
+    shouldDependOn(cells: Array<string>) {
+      containsMany(
+        dependsOn,
+        cells,
+      );
+      return expectFuncs;
+    },
+    shouldBeDependedOnBy(cells: Array<string>) {
+      containsMany(
+        dependingOn,
+        cells,
+      );
+      return expectFuncs;
+    }
+  };
+  return expectFuncs;
+}
 
 describe('jsutils', () => {
-  it('should work', () => {
-    expect(jsutils()).toEqual('jsutils');
+  describe('analyseCellDependancies', () => {
+    it('should be depended on when assigning to another cell', () => {
+      cellWithCode('X1 = 123;')
+        .shouldDependOn([])
+        .shouldBeDependedOnBy(['X1']);
+    });
+    it('should depend on another cell when referencing it - in a return statement', () => {
+      cellWithCode('return X1;')
+        .shouldDependOn(['X1'])
+        .shouldBeDependedOnBy([]);
+    });
+    it('should depend on another cell when referencing it - in a template literal', () => {
+      cellWithCode('let test = `${X1}`')
+        .shouldDependOn(['X1'])
+        .shouldBeDependedOnBy([]);
+    });
+    it('should not depend on another cell when the cell is mentioned in a comment', () => {
+      cellWithCode('// let test = X1;')
+        .shouldDependOn([])
+        .shouldBeDependedOnBy([]);
+    });
+    it('should not depend on a cell when the cells id is declared as a variable and not referenced', () => {
+      cellWithCode('let X1 = 123;')
+        .shouldDependOn([])
+        .shouldBeDependedOnBy([]);
+    });
+    it.skip('should not depend on a cell when the cells id is declared as a variable and then referenced', () => {
+      cellWithCode(
+        `function test() {
+          var X1 = 123;
+          return X1;
+        }`
+      )
+        .shouldDependOn([])
+        .shouldBeDependedOnBy([]);
+    });
+    it.skip('should not be a dependency of another cell when the cells id is declared as a variable and then assigned to', () => {
+      cellWithCode(
+        `function test() {
+          var X1 = 123;
+          X1 = 321;
+        }`
+      )
+        .shouldDependOn([])
+        .shouldBeDependedOnBy([]);
+    });
   });
 });
--- libs/jsutils/src/lib/jsutils.ts
+++ libs/jsutils/src/lib/jsutils.ts
@@ -1,3 +1,61 @@
-export function jsutils(): string {
-  return 'jsutils';
+import { parse, Node, Options } from 'acorn';
+import { fullAncestor, FullAncestorWalkerCallback } from 'acorn-walk';
+
+const PARSE_OPTIONS: Options = {
+  ecmaVersion: 'latest',
+  allowAwaitOutsideFunction: true,
+  allowReturnOutsideFunction: true,
+};
+
+function validateCellId(id: string) {
+  return /^[A-Z]+\d+$/gm.test(id);
+}
+
+function getNodeText(code: string, node: Node) {
+  return code.substr(node.start, node.end - node.start);
+}
+
+function getParent<T>(ancestors: T[]) {
+  return ancestors[ancestors.length - 2];
+}
+
+export function analyseCellDependancies(code: string) {
+  const dependsOn: Array<string> = [];
+  const dependingOn: Array<string> = [];
+
+  const baseNode = parse(code, PARSE_OPTIONS);
+
+  function analyseVariablePattern(node: Node, ancestors: Node[], type: string) {
+    const nodeText = getNodeText(code, node);
+    if (!validateCellId(nodeText)) return;
+    if (getParent(ancestors).type === 'AssignmentExpression') {
+      dependingOn.push(nodeText);
+    }
+  }
+
+  function analyseIdentifier(node: Node, ancestors: Node[], type: string) {
+    const nodeText = getNodeText(code, node);
+    if (!validateCellId(nodeText)) return;
+    dependsOn.push(nodeText);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  function analyseNode(...[node, state, ancestors, type]: Parameters<FullAncestorWalkerCallback<unknown>>) {
+    switch (type) {
+      case 'VariablePattern':
+        return analyseVariablePattern(node, ancestors, type);
+      case 'Identifier':
+        return analyseIdentifier(node, ancestors, type);
+    }
+  }
+
+  fullAncestor(
+    baseNode,
+    analyseNode,
+  );
+
+  return {
+    dependsOn,
+    dependingOn,
+  }
 }
