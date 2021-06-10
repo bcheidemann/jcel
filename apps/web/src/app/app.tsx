40761aca157c31f4e35ed88644f274c07cabef52
bcheidemann <ben@heidemann.co.uk>
2021-06-10 08:53:32 +0100

    fix: basic layout

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Editor, Sheet } from '@jcel/ui';
 
 export function App() {
   return (
-    <div>
-      Test
+    <div style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, display: 'flex' }}>
+      <Sheet />
+      <div style={{ width: 500, backgroundColor: 'white', display: 'flex', flexDirection: 'column' }}>
+        <Editor />
+        <button>Save</button>
+      </div>
     </div>
   );
 }
